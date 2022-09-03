395cbe274734cb4cbe0503ba33184d6fdfd18a83
HyperV <HyperV@JLE106D53.alunos.sc.senac.br>
2022-09-02 22:27:23 -0300

    Upando projeto de carro

---
+++ MeuSiteCarro/MeuSiteCarro/Views/Ficha Técnica/FichaTecnica.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeuSiteCarro.Views.Ficha_Técnica
+{
+    public class FichaTecnicaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MeuSiteCarro/MeuSiteCarro/Views/Modelos/modelos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeuSiteCarro.Views.Modelos
+{
+    public class modelosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
